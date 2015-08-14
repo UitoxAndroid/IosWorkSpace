46b9b1d3d3ed1946bfdd1a057a2ae63c4a05d179
uitox_macbook_jerry <jerry.liao@uitox.com>
2015-08-14 11:05:41 +0800

    加入使用objective-c的bridge

--- ASAP/ASAP/FirstViewController.swift
+++ ASAP/ASAP/FirstViewController.swift
@@ -15,6 +15,23 @@ class FirstViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 //		let log = Logger()
 
+		let url = "http://api.openweathermap.org/data/2.5/weather?lat=37.785834&lon=-122.406417&units=imperial"
+
+		ApiManager<TestDataModel>.action(url, param: nil) { (response: TestDataModel?, error: NSError?, isSuccess:Bool) -> Void in
+			println("name:\(response!.name!)")
+			println("cod: \(response!.cod!)")
+			println("date: \(response!.date!)")
+			println("main.humidity: \(response!.humidity!)")
+			println("main.pressure: \(response!.pressure!)")
+
+			if let weather = response?.weather {
+				for wea in weather {
+					println(wea.description!)
+					println(wea.icon!)
+				}
+			}
+		}
+
 	}
 
 	override func didReceiveMemoryWarning() {
