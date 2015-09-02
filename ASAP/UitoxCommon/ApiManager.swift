82026277060ea8f7cfb75ebf3166d67843645486
uitox_macbook_jerry <jerry.liao@uitox.com>
2015-09-02 14:42:23 +0800

    為了支援Https，ApiManager加上ServerTrustPolicy

--- ASAP/UitoxCommon/ApiManager.swift
+++ ASAP/UitoxCommon/ApiManager.swift
@@ -10,8 +10,24 @@ import Foundation
 
 class ApiManager<T:Mappable>
 {
+	static func resetTrustPolicy() -> Void {
+		let serverTrustPolicy = ServerTrustPolicy.PinCertificates(
+			certificates: ServerTrustPolicy.certificatesInBundle(),
+			validateCertificateChain: true,
+			validateHost: true
+		)
+
+		let serverTrustPolicies: [String: ServerTrustPolicy] = [
+			"uxapi.uitoxbeta.com2": serverTrustPolicy,
+			"uxapi.uitoxbeta.com": .DisableEvaluation
+		]
+
+		Manager.sharedInstance.session.serverTrustPolicyManager = ServerTrustPolicyManager(policies: serverTrustPolicies)
+	}
+
 	static func postDictionary(urlPath:String, params:[String:AnyObject]?, completionHandler: (mapperObject: T?, errorMessage:String?) -> Void) {
-		request(.POST, urlPath, parameters: params).responseObject {
+		resetTrustPolicy()
+		Manager.sharedInstance.request(.POST, urlPath, parameters: params).responseObject {
 			(responseEntity: T?, error: NSError?) in
 			if responseEntity == nil || error != nil {
 				completionHandler(mapperObject:nil, errorMessage:error?.localizedDescription)
@@ -22,6 +38,7 @@ class ApiManager<T:Mappable>
 	}
 
 	static func postArray(urlPath:String, params:[String:AnyObject]?, completionHandler: (mapperObject: [T]?, errorMessage:String?) -> Void) {
+		resetTrustPolicy()
 		request(.POST, urlPath, parameters: params).responseArray {
 			(responseEntity: [T]?, error: NSError?) in
 			if responseEntity == nil || error != nil {
