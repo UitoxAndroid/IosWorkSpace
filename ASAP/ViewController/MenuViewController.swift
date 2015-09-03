0766b311f63ca6056de265d6b65f909c39ebeeeb
uitox_macbook_jerry <jerry.liao@uitox.com>
2015-09-03 15:44:31 +0800

    加入畫面-選單裡的分類

---
+++ ASAP/ViewController/MenuViewController.swift
@@ -0,0 +1,142 @@
+//
+//  MenuViewController.swift
+//  ASAP
+//
+//  Created by uitox_macbook on 2015/9/2.
+//  Copyright (c) 2015年 uitox. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,SKSTableViewDelegate
+{
+	@IBOutlet weak var leftTableView: UITableView!
+	let kLeftViewWidth = 96
+	var rightTableView: SKSTableView!
+	var contentView: UIView!
+	var categoryView: PGCategoryView!
+	var _contentList:[[String]] = []
+	var contentList: [[String]] {
+		if _contentList.count == 0 {
+
+			_contentList.append(["Section0_Row0", "Row0_Subrow1","Row0_Subrow2"])
+			_contentList.append(["Section0_Row1", "Row1_Subrow1", "Row1_Subrow2", "Row1_Subrow3", "Row1_Subrow4", "Row1_Subrow5", "Row1_Subrow6", "Row1_Subrow7", "Row1_Subrow8", "Row1_Subrow9", "Row1_Subrow10", "Row1_Subrow11", "Row1_Subrow12"])
+			_contentList.append(["Section0_Row2"])
+			_contentList.append(["Section1_Row0", "Row0_Subrow1", "Row0_Subrow2", "Row0_Subrow3"])
+			_contentList.append(["Section1_Row1"])
+			_contentList.append(["Section1_Row2", "Row2_Subrow1", "Row2_Subrow2", "Row2_Subrow3", "Row2_Subrow4", "Row2_Subrow5"])
+		}
+
+		return _contentList
+	}
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		contentView = UIView()
+		contentView.frame = CGRect(x: self.view.bounds.size.width, y: 20 + 44, width: self.view.bounds.size.width -  CGFloat(kLeftViewWidth), height: self.view.bounds.size.height - 44 - 20 - 44)
+		contentView.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+		contentView.backgroundColor = UIColor.redColor()
+		self.view.addSubview(contentView)
+
+		rightTableView = SKSTableView()
+		rightTableView.frame = CGRect(x: 0, y: 0, width: contentView.bounds.size.width, height: contentView.bounds.size.height)
+		rightTableView.SKSTableViewDelegate = self
+		rightTableView.autoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth
+		rightTableView.tableFooterView = UIView()
+		contentView.addSubview(rightTableView)
+
+		var viewFrame = contentView.frame;
+		viewFrame.origin.x -= 30
+		viewFrame.size.width = 30;
+		categoryView = PGCategoryView.categoryRightView(contentView)
+		categoryView.frame = viewFrame
+		self.view.addSubview(categoryView)
+
+        // Do any additional setup after loading the view.
+    }
+
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		if tableView == leftTableView {
+			return 1
+		} else {
+			return 1
+		}
+	}
+
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if tableView == leftTableView {
+			return 10
+		} else {
+			return self.contentList.count
+		}
+	}
+
+	func tableView(tableView: SKSTableView!, numberOfSubRowsAtIndexPath indexPath: NSIndexPath!) -> Int {
+		return self.contentList[indexPath.row].count - 1
+	}
+
+	let CellIdentifier = "Cell"
+	let SKSCellIdentifier = "SKSTableViewCell"
+	let SubCellIdentifier = "SubCell"
+
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		if tableView == leftTableView {
+
+			var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as? UITableViewCell
+
+			if cell == nil {
+				cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: CellIdentifier)
+			}
+
+			cell?.textLabel!.text = "left"
+			return cell!
+		} else {
+			var cell: SKSTableViewCell? = tableView.dequeueReusableCellWithIdentifier(SKSCellIdentifier) as? SKSTableViewCell
+
+			if cell == nil {
+				cell = SKSTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: SKSCellIdentifier)
+			}
+
+			cell?.textLabel!.text = self.contentList[indexPath.row][0]
+
+			if (self.contentList[indexPath.row].count > 1) {
+				cell?.isExpandable = true
+			} else {
+				cell?.isExpandable = false
+			}
+
+			return cell!
+		}
+	}
+
+	func tableView(tableView: SKSTableView!, cellForSubRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+
+		var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(SubCellIdentifier) as? UITableViewCell
+
+		if cell == nil {
+			cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: SubCellIdentifier)
+		}
+
+		println("row:\(indexPath.row)")
+		println("subRow:\(indexPath.subRow)")
+
+		var s = self.contentList[indexPath.row][indexPath.subRow]
+		cell?.textLabel?.text = "\(s)"
+		cell?.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+
+		return cell!
+	}
+
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		tableView.deselectRowAtIndexPath(indexPath, animated: true)
+		if tableView == leftTableView {
+			categoryView.show()
+		} else {
+			print(indexPath.row)
+		}
+	}
+
+
+}
